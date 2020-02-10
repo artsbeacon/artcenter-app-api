c0dea0fda00298c1d2a918e36c14521874e74edf
Jeremy Dost <jdost@sharpdotinc.com>
2020-02-10 09:20:00 -0500

    get artworks api working

--- services/artworks-api/list.js
+++ services/artworks-api/list.js
@@ -1,5 +1,5 @@
-import * as dynamoDbLib from "./libs/dynamodb-lib";
-import { success, failure } from "./libs/response-lib";
+import * as dynamoDbLib from "../../libs/dynamodb-lib";
+import { success, failure } from "../../libs/response-lib";
 
 export async function main(event, context) {
   const params = {
@@ -21,6 +21,6 @@ export async function main(event, context) {
     // Return the matching list of items in response body
     return success(result.Items);
   } catch (e) {
-    return failure({ status: false });
+    return failure({ status: false, error: e });
   }
 }
